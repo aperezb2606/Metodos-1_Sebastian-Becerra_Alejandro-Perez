f34ef3f4bb2a53e51f9774e9901bd463937c3fda
aperezb2606 <123389252+aperezb2606@users.noreply.github.com>
2023-02-17 16:55:08 -0500

    Update Valor factorial.py

--- Taller 1/Valor factorial.py
+++ Taller 1/Valor factorial.py
@@ -7,11 +7,4 @@ def funcion_factorial(natural:int)->int:
         natural-=1
         natural_constante*=natural
     return natural_constante
-"Punto a del ejercicio 6"
-x= np.linspace(-4,4,25)
-y= np.linspaces(-4,4,25)
-"Punto b del ejercicio 6"
-def Function(x):
-    return 2*x(1-((4)/(x**2+y**2)))
-"Punto c del ejercicio 6"
 
